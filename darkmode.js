fb2d2203fa85cc139b98f391575499c0a3e1d0b9
sarahneri <105995227+sarahneri@users.noreply.github.com>
2023-01-11 17:34:42 -0300

    Add files via upload

---
+++ darkmode.js
@@ -0,0 +1,8 @@
+button onclick = "myFunction()" > Toggle dark mode</button >
+
+	<script>
+		function myFunction() {
+		var element = document.body;
+		element.classList.toggle("dark-mode");
+	}
+</script>
